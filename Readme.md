845b00df6c77fd09504966c0b1d86294a12ab467
Danilo Cuculic <danilo.cuculic@gmail.com>
2014-01-10 00:08:53 +0100

    v0.9.1: geocomplete search field with Places library for GoogleMaps

--- Readme.md
+++ Readme.md
@@ -2,26 +2,34 @@ GoogleMaps widget for ManagerManager plugin
 ===========================================
 
 Integrates interactive map widget into MODX CMF backend resource management
-including two-way synchronization with the geolocation template variable.
+including two-way synchronization with the geolocation template variable
+and autocompleting search field based on Places library for GoogleMaps.
 
 
 Usage
 -----
-
-
+```php
+mm_widget_googlemap("tvlatlong" ...);
+```
 
 Contributors
 ------------
 
-- Oori
-  - initial GoogleMaps v2 API widget prototype
+# Oori
+* initial GoogleMaps v2 API widget prototype
 
-- DivanDesign (http://code.divandesign.biz/modx/mm_ddgmap)
-  - backend UI styling
+# DivanDesign (http://code.divandesign.biz/modx/mm_ddgmap)
+* backend UI styling
+
+# UbiLabs (https://github.com/ubilabs/geocomplete)
+* GeoComplete jQuery plugin
 
 
 Changelog
 ---------
 
-- 0.9.0 (2014-01-09)
-  - Initial widget based on GoogleMaps API v3
+# 0.9.1 (2014-01-10)
+* added geolocation search field with jQuery plugin GeoComplete v1.4.1
+
+# 0.9.0 (2014-01-09)
+* initial widget based on GoogleMaps API v3
--- googlemap.php
+++ googlemap.php
@@ -1,7 +1,7 @@
 <?php
 /* mm_widget_googlemap
- * @version 0.9.0
- * @description Shows an interactive Google Map with two-way geolocation sync
+ * @version 0.9.1
+ * @description Shows an interactive Google Map with two-way geolocation sync and autocompleted PoI search field
  * @author Danilo Cuculić (eoler@castus.me)
  *
  * @param $mapw {string; integer} - width of the map, default: auto (whole container)
@@ -10,8 +10,8 @@
  */
 function mm_widget_googlemap($field, $mapw='auto', $maph='450', $mapdefloc='52.5,13.5', $roles='', $templates='') {
  global $modx, $mm_fields, $modx_lang_attribute;
-  $evt = &$modx->Event;
 
+  $evt = &$modx->Event;
   if ($evt->name == 'OnDocFormRender' && useThisRule($roles, $templates)) {
     $fieldName = $mm_fields[$field]['fieldname'];
     if (intval($mapw) > 0) $mapw .= "px";
@@ -20,19 +20,20 @@ function mm_widget_googlemap($field, $mapw='auto', $maph='450', $mapdefloc='52.5
 		$output .= includeJs($modx->config['base_url'] .'assets/plugins/managermanager/widgets/googlemap/googlemap.js');
     $output .= "      var elmfield = \$j('#{$fieldName}');
       var elmparent = elmfield.parents('tr:first');
-      var elmsection = \$j('<div class=\"sectionHeader\">' + elmparent.find('.warning').text() + '</div><div class=\"sectionBody tmplvars\"><div class=\"mmwidgetGoogleMap_{$fieldName}\" style=\"position:relative; width:{$mapw}; height:{$maph};\"></div></div>');
+      var elmsection = \$j('<div class=\"sectionHeader\">'+ elmparent.find('.warning').text() +'</div><div id=\"sect_{$fieldName}\" class=\"sectionBody tmplvars\"><div class=\"mmwidgetGoogleMap_{$fieldName}\" style=\"position:relative; width:{$mapw}; height:{$maph};\"></div></div>');
       elmparent.hide().prev('tr').hide();
       elmfield.parents('.tab-page:first').append(elmsection);
       elmfield.insertBefore('.mmwidgetGoogleMap_{$fieldName}');
+      \$j('<input type=\"text\" id=\"geoloc_{$fieldName}\" name=\"geoloc_{$fieldName}\" tvtype=\"text\" style=\"width:60%; float:right;\">').insertAfter(elmfield);;
       window.pagemapsloaded = function(){ googlemaptv('{$fieldName}', elmsection, '{$mapdefloc}'); };
     function loadGmapscript(){
       var script = document.createElement('script');
       script.type = 'text/javascript';
-      script.src = '//maps.google.com/maps/api/js?v=3&sensor=false&callback=pagemapsloaded&language={$modx_lang_attribute}';
+      script.src = '//maps.google.com/maps/api/js?v=3&sensor=false&callback=pagemapsloaded&language={$modx_lang_attribute}&libraries=places';
       document.body.appendChild(script);
     }
-    window.onload = loadGmapscript;";
-    $evt->output($output ."\n");
+    window.onload = loadGmapscript;\n";
+    $evt->output($output);
 	}
 	
 }
