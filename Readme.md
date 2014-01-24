8efa233df5d914e1a4b8d28e53c9025d4a52a698
Danilo Cuculic <danilo.cuculic@gmail.com>
2014-01-24 13:51:10 +0100

    Added configuration options array (Laravel style named parameters)

--- Readme.md
+++ Readme.md
@@ -4,6 +4,7 @@ GoogleMaps widget for ManagerManager plugin
 Integrates interactive map widget into MODX CMF backend resource management
 including two-way synchronization with the geolocation template variable
 and autocompleting search field based on Places library for GoogleMaps.
+Requires PHP5.3+
 
 
 Usage
@@ -16,24 +17,22 @@ mm_widget_googlemap("tplvar_latlong" ...);
 Contributors
 ------------
 
-## Oori
+### Oori
 * initial GoogleMaps v2 API widget prototype
-
-## DivanDesign (http://code.divandesign.biz/modx/mm_ddgmap)
+### DivanDesign (http://code.divandesign.biz/modx/mm_ddgmap)
 * backend UI styling
-
-## UbiLabs (https://github.com/ubilabs/geocomplete)
+### UbiLabs (https://github.com/ubilabs/geocomplete)
 * GeoComplete jQuery plugin
 
 
 Changelog
 ---------
 
-## 0.9.2 (2014-01-21)
+### 0.9.2 (2014-01-22)
 - added business establishments to searched locations
 
-## 0.9.1 (2014-01-10)
+### 0.9.1 (2014-01-10)
 - added geolocation search field with jQuery plugin GeoComplete v1.4.1
 
-## 0.9.0 (2014-01-09)
+### 0.9.0 (2014-01-09)
 - initial widget based on GoogleMaps API v3
--- googlemap.php
+++ googlemap.php
@@ -1,31 +1,45 @@
 <?php
 /* mm_widget_googlemap
- * @version 0.9.2
- * @description Shows an interactive Google Map with two-way geolocation sync and autocompleted PoI search field
+ * @version 0.9.9
+ * @description upgrades template variable by showing an interactive Google Map and autocompleted geolocation search field
  * @author Danilo Cuculić (eoler@castus.me)
  *
- * @param $mapw {string; integer} - width of the map, default: auto (whole container)
- * @param $maph {integer} - height of the map control, default: 450 (px)
- * @param $mapdefloc {string} - center map on latlng if location field empty, default: Rijeka, Croatia
+ * @param $options {string} - parameters passed to map UI
  */
-function mm_widget_googlemap($field, $mapw='auto', $maph='450', $mapdefloc='52.5,13.5', $roles='', $templates='') {
+function mm_widget_googlemap($field, $roles='', $templates='', array $config=null) {
  global $modx, $mm_fields, $modx_lang_attribute;
+  static $defmapopts = array(
+    'width' => "auto",
+    'height' => "450",
+    'center' => "52.5,13.5", // default: Rijeka, Croatia
+    'gmapOptions' => array(
+      'minZoom' => 15,
+      'zoom' => 15,
+      'maxZoom' => 25,
+      'draggable' => true,
+      'scrollwheel' => true
+    )
+  );
 
   $evt = &$modx->Event;
   if ($evt->name == 'OnDocFormRender' && useThisRule($roles, $templates)) {
     $fieldName = $mm_fields[$field]['fieldname'];
-    if (intval($mapw) > 0) $mapw .= "px";
-    if (intval($maph) > 0) $maph .= "px";
+    $mapopts = $defmapopts;
+    if (is_array($config)) {
+      $mapopts = array_replace_recursive($defmapopts, $config);
+    }
+    if (intval($mapopts['width']) > 0) $mapopts['width'] .= "px";
+    if (intval($mapopts['height']) > 0) $mapopts['height'] .= "px";
 		$output = "//  -------------- GoogleMap widget ------------- \n";
 		$output .= includeJs($modx->config['base_url'] .'assets/plugins/managermanager/widgets/googlemap/googlemap.js');
     $output .= "      var elmfield = \$j('#{$fieldName}');
       var elmparent = elmfield.parents('tr:first');
-      var elmsection = \$j('<div class=\"sectionHeader\">'+ elmparent.find('.warning').text() +'</div><div id=\"sect_{$fieldName}\" class=\"sectionBody tmplvars\"><div class=\"mmwidgetGoogleMap_{$fieldName}\" style=\"position:relative; width:{$mapw}; height:{$maph};\"></div></div>');
+      var elmsection = \$j('<div class=\"sectionHeader\">'+ elmparent.find('.warning').text() +'</div><div id=\"sect_{$fieldName}\" class=\"sectionBody tmplvars\"><div class=\"mmwidgetGoogleMap_{$fieldName}\" style=\"position:relative; width:{$mapopts['width']}; height:{$mapopts['height']};\"></div></div>');
       elmparent.hide().prev('tr').hide();
       elmfield.parents('.tab-page:first').append(elmsection);
       elmfield.insertBefore('.mmwidgetGoogleMap_{$fieldName}');
-      \$j('<input type=\"text\" id=\"geoloc_{$fieldName}\" name=\"geoloc_{$fieldName}\" tvtype=\"text\" style=\"width:60%; float:right;\">').insertAfter(elmfield);;
-      window.pagemapsloaded = function(){ googlemaptv('{$fieldName}', elmsection, '{$mapdefloc}'); };
+      \$j('<input type=\"text\" id=\"geoloc_{$fieldName}\" name=\"geoloc_{$fieldName}\" tvtype=\"text\" style=\"width:60%; float:right;\">').insertAfter(elmfield);
+      window.pagemapsloaded = function(){ googlemaptv('{$fieldName}', elmsection, '{$mapopts['center']}'); };
     function loadGmapscript(){
       var script = document.createElement('script');
       script.type = 'text/javascript';
